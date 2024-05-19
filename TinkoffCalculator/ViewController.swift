9ac7f1cac90c1fd714c2b7cd2bf5a810afdebff8
Dima <dorogovd@yandex.ru>
2024-05-19 19:24:32 +0300

    bug fixes

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,19 +7,136 @@
 
 import UIKit
 
+enum CalculationError: Error {
+    case dividedByZero
+}
+
+enum Operation: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "x"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+        case .substract:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        }
+    }
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
 class ViewController: UIViewController {
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
-        print(buttonText)
         
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if buttonText == "," && label.text == "0" || buttonText == "," && label.text == "Ошибка" {
+            label.text = "0,"
+//        } else if buttonText == "+" || buttonText == "-" || buttonText == "x" || buttonText == "/" {
+//            label.text == label.text
+        } else if label.text == "Ошибка" {
+            label.text = buttonText
+        } else if label.text == "0" {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
     }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard 
+            let buttonText = sender.currentTitle,
+            let buttonOperation = Operation(rawValue: buttonText)
+            else { return }
+        guard
+            let labelText = label.text, // конвертируем текстлэйбл в число
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+            else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+  
+        resetTextLabel()
+    }
+    
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        resetTextLabel()
+    }
+    
+    @IBAction func calculateButtonPressed() {
+        guard
+            let labelText = label.text, // конвертируем текстлэйбл в число
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+            else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            
+            label.text = numberFormatter.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Ошибка"
+        }
+        calculationHistory.removeAll()
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var calculationHistory: [CalculationHistoryItem] = []
+    
+    // ленивое свойство (в отличии от обычного) устанавливает своё значение только после обращения к нему и в дальнейшем это значение не изменяется
+    lazy var numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.usesGroupingSeparator = false
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        resetTextLabel()
+    }
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        var currentResult = firstNumber
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+                else { break }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        return currentResult
     }
 
+    func resetTextLabel() {
+        label.text = "0"
+    }
 
 }
 
