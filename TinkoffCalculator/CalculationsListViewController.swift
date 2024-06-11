47709ac227de02c42fdeb347b6f6d2de84202562
Dima <dorogovd@yandex.ru>
2024-06-11 19:59:39 +0300

    history of results

--- TinkoffCalculator/CalculationsListViewController.swift
+++ TinkoffCalculator/CalculationsListViewController.swift
@@ -9,5 +9,24 @@ import UIKit
 
 class CalculationsListViewController: UIViewController {
     
+    var result: String?
     
+    @IBOutlet weak var calculationLabel: UILabel!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    private func initialize() {
+        modalPresentationStyle = .fullScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculationLabel.text = result
+    }
 }
